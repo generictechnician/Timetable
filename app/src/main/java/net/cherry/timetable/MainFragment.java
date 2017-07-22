3bc492ffced780b187211fda3c35f9c29ab4586f
generictechnician <generictechnician@users.noreply.github.com>
2017-07-22 11:08:12 +0100

    initial commit

---
+++ app/src/main/java/net/cherry/timetable/MainFragment.java
@@ -0,0 +1,28 @@
+package net.cherry.timetable;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.provider.Settings;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainFragment extends Fragment {
+
+    public MainFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+
+    }
+}
